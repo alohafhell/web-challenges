306775a7aeeddcf50a321d45139bf43f7112612f
alissa svertokina <svertokina92@gmail.com>
2025-03-12 20:09:45 +0100

    Switch Values completion

--- js_inputs_and_strings/js-inputs-and-strings_switch-values/js/index.js
+++ js_inputs_and_strings/js-inputs-and-strings_switch-values/js/index.js
@@ -1,8 +1,11 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -10,4 +13,8 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  [firstInput.value, secondInput.value] = [secondInput.value, firstInput.value];
 });
