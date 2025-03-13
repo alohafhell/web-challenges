f4d9f9326bca7ae5181c6823eab0186ba4f499c2
alissa svertokina <svertokina92@gmail.com>
2025-03-13 14:02:00 +0100

    Social Media complete

--- JS_createElement/js-createelement_social-media-post/js/index.js
+++ JS_createElement/js-createelement_social-media-post/js/index.js
@@ -6,7 +6,29 @@ function handleLikeButtonClick(event) {
 }
 
 const likeButton = document.querySelector('[data-js="like-button"]');
-likeButton.addEventListener("click", handleLikeButtonClick);
+if (likeButton) {
+  likeButton.addEventListener("click", handleLikeButtonClick);
+}
 
-// Exercise:
-// Use document.createElement() and append another social media post to the body.
+const articleElement = document.createElement("article");
+articleElement.className = "post";
+
+const paragraphElement = document.createElement("p");
+paragraphElement.className = "post__content";
+paragraphElement.textContent = "Long story short, I survived";
+
+const footerElement = document.createElement("footer");
+footerElement.className = "post__footer";
+
+const spanElement = document.createElement("span");
+spanElement.className = "post__username";
+spanElement.textContent = "@TaylorSwiftovna";
+
+const buttonElement = document.createElement("button");
+buttonElement.className = "post__button";
+buttonElement.textContent = "♥ Like";
+buttonElement.addEventListener("click", handleLikeButtonClick);
+
+articleElement.append(paragraphElement, footerElement);
+footerElement.append(spanElement, buttonElement);
+document.body.append(articleElement);
