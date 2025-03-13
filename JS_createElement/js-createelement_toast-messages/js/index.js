57a9cb5ba858a8344a782d152a68928422c55263
alissa svertokina <svertokina92@gmail.com>
2025-03-13 11:33:49 +0100

    toast challenge complete

--- JS_createElement/js-createelement_toast-messages/js/index.js
+++ JS_createElement/js-createelement_toast-messages/js/index.js
@@ -6,6 +6,10 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("li");
+  newToast.className = "toast-container__message";
+  newToast.textContent = "This is a new toast message!";
+  toastContainer.appendChild(newToast);
 });
 
 clearButton.addEventListener("click", () => {
