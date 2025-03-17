52a90b3c5baca13091fadc6d057b4dc9f55cef9f
alissa svertokina <svertokina92@gmail.com>
2025-03-17 16:18:50 +0100

    Creating list Complete

--- JS-loops/js-loops_forof-list/js/index.js
+++ JS-loops/js-loops_forof-list/js/index.js
@@ -15,6 +15,21 @@ const programmingLanguages = [
   "Ruby",
 ];
 
-// --v-- write or modify code below this line --v--
+for (const programmingLanguage of programmingLanguages) {
+  const li = document.createElement("li");
+  li.textContent = programmingLanguage;
+  ol.append(li);
+  console.log(programmingLanguage);
+}
 
-// --^-- write or modify code above this line --^--
+//An `ol` element that’s been created and appended to the document.
+//- A string array stored in a variable called `programmingLanguages`.
+
+//Your task is to create a list item for each array item and add
+// it to the ordered list. Each list item should display the value of the
+// current array item. Use a `for...of` loop to accomplish this.
+
+//The following hints may guide you:
+
+//- Do you remember the property for setting the text of an HTML element?
+//- Be sure to append each list item to the ordered list.
