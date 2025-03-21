d9db608b684554eb0b86713ddf182562802cb853
alissa svertokina <svertokina92@gmail.com>
2025-03-21 16:06:28 +0100

    fix

--- JS-fetch/js-fetch_star-wars-console/js/index.js
+++ JS-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,13 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
 
+  console.log("Full data object:", data);
+  console.log(data.results[0].name);
+  console.log(data.results[3].eye_color);
+  console.log("R2-D2's eye color:", data.results[3].eye_color);
+}
 fetchData();
