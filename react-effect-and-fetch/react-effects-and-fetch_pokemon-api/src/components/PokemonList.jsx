a2869a4f4ed67b0c998342b213099af061743050
alissa svertokina <svertokina92@gmail.com>
2025-04-01 11:27:51 +0200

    Pokémon API done

--- react-effect-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
+++ react-effect-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
@@ -8,16 +8,18 @@ export default function PokemonList() {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon");
       const data = await response.json();
       setPokemon(data.results);
+      console.log(data.results);
     } catch (error) {
       console.log(error);
     }
   }
 
+  useEffect(() => {
+    loadPokemon();
+  }, []);
+
   return (
     <main>
-      <button type="button" onClick={loadPokemon}>
-        Load Pokémon
-      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
