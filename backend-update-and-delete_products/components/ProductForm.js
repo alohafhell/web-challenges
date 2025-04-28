9573f331aac20c9bd946cc91f509610c30ba1786
Alissa Svertokina <svertokina92@gmail.com>
2025-04-28 11:13:03 +0200

    Add agile

---
+++ backend-update-and-delete_products/components/ProductForm.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import useSWR from "swr";
+import StyledButton from "@/components/Button";
+
+export default function ProductForm({}) {
+  
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledHeading>Add a new Fish</StyledHeading>
+      <StyledLabel htmlFor="name">
+        Name:
+        <input type="text" id="name" name="name" />
+      </StyledLabel>
+      <StyledLabel htmlFor="description">
+        Description:
+        <input type="text" id="description" name="description" />
+      </StyledLabel>
+      <StyledLabel htmlFor="price">
+        Price:
+        <input type="number" id="price" name="price" min="0" />
+      </StyledLabel>
+      <StyledLabel htmlFor="currency">
+        Currency:
+        <select id="currency" name="currency">
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="GBP">GBP</option>
+        </select>
+      </StyledLabel>
+      <StyledButton type="submit">Submit</StyledButton>
+    </StyledForm>
+  );
+}
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+export const StyledHeading = styled.h2`
+  text-align: center;
+  color: var(--color-nemo);
+`;
+
+export const StyledLabel = styled.label`
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
+`;
